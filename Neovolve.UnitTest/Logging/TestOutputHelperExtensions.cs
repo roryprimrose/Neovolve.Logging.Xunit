3b02e6f8aa1ae679f3883bd9e0d1d7dffd49c5d9
Rory Primrose <rory@neovolve.com>
2017-05-30 23:48:43 +1000

    Fixed remaining ILogger<T> signature.

--- Neovolve.UnitTest/Logging/TestOutputHelperExtensions.cs
+++ Neovolve.UnitTest/Logging/TestOutputHelperExtensions.cs
@@ -37,7 +37,7 @@
         /// <param name="output">The test output helper.</param>
         /// <returns>The logger.</returns>
         /// <exception cref="ArgumentNullException">The <paramref name="output" /> is <c>null</c>.</exception>
-        public static ILogger BuildLoggerFor<T>(this ITestOutputHelper output)
+        public static ILogger<T> BuildLoggerFor<T>(this ITestOutputHelper output)
         {
             if (output == null)
             {
