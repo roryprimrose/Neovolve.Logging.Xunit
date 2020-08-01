0c22aba63e012ffc587403fc64b741e4184d48f9
Rory Primrose <rory@neovolve.com>
2020-08-01 16:41:03 +1000

    Added LoggingTestsBase and LoggingTestsBase<T> classes

--- Divergic.Logging.Xunit.UnitTests/ScenarioTests.cs
+++ Divergic.Logging.Xunit.UnitTests/ScenarioTests.cs
@@ -1,144 +1,26 @@
 ﻿namespace Divergic.Logging.Xunit.UnitTests
 {
-    using System;
     using global::Xunit;
     using global::Xunit.Abstractions;
     using Microsoft.Extensions.Logging;
-    using ModelBuilder;
 
-    public class ScenarioTests
+    public class ScenarioTests : LoggingTestsBase
     {
-        private readonly ITestOutputHelper _output;
-
-        public ScenarioTests(ITestOutputHelper output)
+        public ScenarioTests(ITestOutputHelper output) : base(output)
         {
-            _output = output;
         }
 
         [Fact]
-        public void TestOutputWritesMessagesInContextOfScopes()
+        public void TestOutputWritesMessages()
         {
-            var logger = _output.BuildLogger();
+            Logger.LogCritical("Writing critical message");
+            Logger.LogDebug("Writing debug message");
+            Logger.LogError("Writing error message");
+            Logger.LogInformation("Writing information message");
+            Logger.LogTrace("Writing trace message");
+            Logger.LogWarning("Writing warning message");
 
-            logger.LogCritical("Writing critical message");
-            logger.LogDebug("Writing debug message");
-            logger.LogError("Writing error message");
-            logger.LogInformation("Writing information message");
-            logger.LogTrace("Writing trace message");
-            logger.LogWarning("Writing warning message");
-
-            using (logger.BeginScope("First scope"))
-            {
-                logger.LogInformation("Inside first scope");
-
-                using (logger.BeginScope("Second scope"))
-                {
-                    logger.LogInformation("Inside second scope");
-                }
-
-                logger.LogInformation("After second scope");
-            }
-
-            logger.LogInformation("After first scope");
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("  ")]
-        public void TestOutputWritesMessagesUsingScopesWithoutState(string scopeState)
-        {
-            var logger = _output.BuildLogger();
-
-            logger.LogCritical("Writing critical message");
-            logger.LogDebug("Writing debug message");
-            logger.LogError("Writing error message");
-            logger.LogInformation("Writing information message");
-            logger.LogTrace("Writing trace message");
-            logger.LogWarning("Writing warning message");
-
-            using (logger.BeginScope((object) scopeState))
-            {
-                logger.LogInformation("Inside first scope");
-
-                using (logger.BeginScope((object) scopeState))
-                {
-                    logger.LogInformation("Inside second scope");
-                }
-
-                logger.LogInformation("After second scope");
-            }
-
-            logger.LogInformation("After first scope");
-        }
-
-        [Fact]
-        public void TestOutputWritesScopeBoundariesUsingObjects()
-        {
-            var logger = _output.BuildLogger();
-
-            logger.LogCritical("Writing critical message");
-            logger.LogDebug("Writing debug message");
-            logger.LogError("Writing error message");
-            logger.LogInformation("Writing information message");
-            logger.LogTrace("Writing trace message");
-            logger.LogWarning("Writing warning message");
-
-            var firstPerson = Model.Create<Person>();
-
-            using (logger.BeginScope(firstPerson))
-            {
-                logger.LogInformation("Inside first scope");
-
-                var secondPerson = Model.Create<Person>();
-
-                using (logger.BeginScope(secondPerson))
-                {
-                    logger.LogInformation("Inside second scope");
-                }
-
-                logger.LogInformation("After second scope");
-            }
-
-            logger.LogInformation("After first scope");
-        }
-
-        [Fact]
-        public void TestOutputWritesScopeBoundariesUsingValueTypes()
-        {
-            var logger = _output.BuildLogger();
-
-            logger.LogCritical("Writing critical message");
-            logger.LogDebug("Writing debug message");
-            logger.LogError("Writing error message");
-            logger.LogInformation("Writing information message");
-            logger.LogTrace("Writing trace message");
-            logger.LogWarning("Writing warning message");
-
-            using (logger.BeginScope(Guid.NewGuid()))
-            {
-                logger.LogInformation("Inside first scope");
-
-                using (logger.BeginScope(Environment.TickCount))
-                {
-                    logger.LogInformation("Inside second scope");
-                }
-
-                logger.LogInformation("After second scope");
-            }
-
-            logger.LogInformation("After first scope");
-        }
-
-        private class Person
-        {
-            public DateTime DateOfBirth { get; set; }
-
-            public string Email { get; set; }
-
-            public string FirstName { get; set; }
-
-            public string LastName { get; set; }
+            Output.WriteLine("All finished");
         }
     }
 }
